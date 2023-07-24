6f362556471b9d542fad785cb6b726800612385e
caglenoah <caglenoah@gmail.com>
2023-07-24 12:45:36 -0600

    fixed ui logic, doesnt include puntuation" "

--- maingui.js
+++ maingui.js
@@ -1,7 +1,7 @@
 const countWords = () => {
   
     let string = document.querySelector(".user-input").value
-    let wordList = string.toLowerCase().replace(/\s/g, "").split("")
+    let wordList = string.toLowerCase().match(/[a-z]/gi)
     document.querySelector(".word-output").innerHTML = JSON.stringify(
       wordList.reduce((acc, word) => {
         acc[word] = (acc[word] || 0) + 1
@@ -13,3 +13,7 @@ const countWords = () => {
       .replace(/[{}]+/g, "")
   }
   
+
+  //gi
+  //g flag = means global
+  //i flag- case insensitive
