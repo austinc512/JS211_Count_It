6906c5150925f58eb3d7ad53e94e9890427f7e98
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-07-24 14:41:39 -0500

    fixing stuff

--- maingui.js
+++ maingui.js
@@ -1,19 +1,17 @@
 const countWords = () => {
-  
-    let string = document.querySelector(".user-input").value
-    let wordList = string.toLowerCase().match(/[a-z]/gi)
-    document.querySelector(".word-output").innerHTML = JSON.stringify(
-      wordList.reduce((acc, word) => {
-        acc[word] = (acc[word] || 0) + 1
-        return acc
-      }, {})
-    )
-      .replace(/[, ]+/g, " ")
-      .replace(/['"]+/g, "")
-      .replace(/[{}]+/g, "")
-  }
-  
+  let string = document.querySelector(".user-input").value;
+  let wordList = string.toLowerCase().match(/[a-z]/gi);
+  document.querySelector(".word-output").innerHTML = JSON.stringify(
+    wordList.reduce((acc, word) => {
+      acc[word] = (acc[word] || 0) + 1;
+      return acc;
+    }, {})
+  )
+    .replace(/[, ]+/g, " ")
+    .replace(/['"]+/g, "")
+    .replace(/[{}]+/g, "");
+};
 
-  //gi
-  //g flag = means global
-  //i flag- case insensitive
+//gi
+//g flag = means global
+//i flag- case insensitive
--- test.js
+++ test.js
@@ -1,41 +1,30 @@
-
-/** 
+/**
  * 1. Define function, "countWords"
  * 2. Convert input 'string to lowercase, make case-insensitive
  * 3. Use .replace method with regex "/\s/g" to remove white space
  * 4. Use .split to separate to individual characters
- * 5. Callback function: 
+ * 5. Callback function:
  * .reduce takes callback with 2 arguments: "acc" and the current letter "word".
- * 
-*/
+ *
+ */
 
-const countWords = string => {
-    let wordList = string.toLowerCase().replace(/\s/g, '').split('')
-    return wordList.reduce((acc, word) => {
-      acc[word] = (acc[word] || 0) + 1
-       // incremements the count of the current letter "word" in the
-       // accumulator object "acc". If letter doesn't exist in "acc",
-       // it sets its count to 1.
-      return acc
+const countWords = (string) => {
+  let wordList = string.toLowerCase().match(/[a-z]/gi);
+  return JSON.stringify(
+    wordList.reduce((acc, word) => {
+      acc[word] = (acc[word] || 0) + 1;
+      return acc;
     }, {})
-  }
-  
-  countWords('How many of each letter are there in this string')
-  //Returns an objet representing the frequencies of each letter in string.
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  )
+    .replace(/[, ]+/g, " ")
+    .replace(/['"]+/g, "")
+    .replace(/[{}]+/g, "");
+};
+
+console.log(countWords("How many of each letter are there in this string"));
+
+//Returns an objet representing the frequencies of each letter in string.
+//does it produce valid output from a string
+//doesnt take capitolization into consideration
+//does not include punctuation
+// does not include spaces
